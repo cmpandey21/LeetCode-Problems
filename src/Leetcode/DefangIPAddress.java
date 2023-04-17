79756af7732b3f8cd96693473b39f38805149397
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-04-17 09:40:08 +0530

    New Programs 17th-April-2023

---
+++ src/Leetcode/DefangIPAddress.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+public class DefangIPAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s=defangIPAddress("1.1.1.1");
+		System.out.println(s);
+	}
+	
+	public static String defangIPAddress(String address)
+	{
+
+        String str="";
+
+        for(int i=0;i<address.length();i++)
+        {
+            if(address.charAt(i)=='.')
+            {
+str=str+"["+address.charAt(i)+"]";
+            }
+
+            else
+            {
+                str=str+address.charAt(i);
+            }
+        }
+		return str;
+		
+	}
+
+}
---
+++ src/Leetcode/LongestPalindromicSubsequence.java
@@ -0,0 +1,43 @@
+package Leetcode;
+
+/*
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+ */
+
+public class LongestPalindromicSubsequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int s=longestPalindromeSubseq("bbbab");
+		int s1=longestPalindromeSubseq("aabaa");
+		System.out.println(s);
+		System.out.println(s1);
+	}
+	
+	public static int longestPalindromeSubseq(String s) {
+	     int count=0;
+	     if(s.length()==1)
+	     {
+	    	 return 1;
+	     }
+	     else
+	    	 
+	     for(int i=0;i<s.length();i++)
+	     {
+	    	 for(int j=i+1;i<s.length();i++)
+	    	 {
+	         if(s.charAt(i)==s.charAt(j))
+	         {
+	             count++;
+	         }}
+
+	     }
+	     
+	     return count;
+
+}
+	
+}
