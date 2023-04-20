3bee69e57fa14c23418c92e0096b1e539d927d07
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-04-20 10:47:57 +0530

    New Programs 20th-April-2023

---
+++ src/Leetcode/ConcatenationOfArray.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+/*
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+ */
+public class ConcatenationOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] nums = {1,2,1};
+		int [] ans=getConcatenation(nums);
+		display(ans);
+	}
+	public static void display(int[] ans)
+	{
+		for(int i=0;i<ans.length;i++)
+		{
+			System.out.print(ans[i]+" ");
+		}
+	}
+	
+	 public static int[] getConcatenation(int[] nums) {
+		 int n=nums.length;
+		 int [] ans=new int[n*2];
+		 
+		 
+		 for(int i=0;i<n;i++)
+		 {
+			 ans[i]=nums[i];
+			 ans[i+n]=nums[i];
+		 }
+		 
+		 return ans;
+	        
+	    }
+
+}
