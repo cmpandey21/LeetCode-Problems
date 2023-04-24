ed71173368e094ed557b7874a5b8e3cb6f95dc2c
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-04-24 10:52:44 +0530

    New Programs 24th-April-2023

---
+++ src/Leetcode/ReverseString.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		char [] s = {'h','e','l','l','o'};
+		reverseString(s); 
+	
+		
+	}
+	
+	public static void reverseString(char[] s) {
+        int n=s.length;
+ 
+        int j=0;
+        for(int i=n-1;i>j;i--)
+        {
+        	char temp=s[j];
+        	s[j++]=s[i];
+           s[i]=temp;
+        }
+        
+        System.out.println(Arrays.toString(s));
+       
+        
+        
+    }
+	
+
+}
