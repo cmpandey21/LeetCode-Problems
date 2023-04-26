5add65af37ac2d1406a24b6901ed34209902a4fd
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-04-26 20:42:03 +0530

    New Programs 26th-April-2023

---
+++ src/Leetcode/BalancedStringSplit.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+/*
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+ */
+
+public class BalancedStringSplit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(balancedStringSplit("RLRRLLRLRL"));
+		
+	}
+	
+	public static int balancedStringSplit(String s)
+	{
+		int count=0, ans=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='L')
+			{
+				count++;
+			}
+			
+			else {
+				count--;
+			}
+			
+			if(count==0)
+			{
+				ans++;
+			}
+		}
+		return ans;
+	}
+	
+
+}
