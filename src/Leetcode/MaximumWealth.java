8223ffe9e279f8be6a8dd65292d43f95230ff577
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-05-08 16:28:54 +0530

    New Programs 8th-May-2023

---
+++ src/Leetcode/MaximumWealth.java
@@ -0,0 +1,44 @@
+package Leetcode;
+
+/*
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+ */
+public class MaximumWealth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[][] = {{1,5},{7,3},{3,5}};// { { 1, 2, 3 }, { 3, 2, 1 } };
+		int result = maximumWealth(arr);
+		System.out.println(result);
+
+	}
+
+	public static int maximumWealth(int[][] accounts) {
+		int max=0,result = 0;
+		for(int i=0;i<accounts.length;i++)
+		{
+			int sum=0;
+			for(int j=0;j<accounts[i].length;j++)
+			{
+				sum=sum+accounts[i][j];
+				
+			}
+		
+			
+			
+			if(sum>=max)
+			{
+				result=sum;
+				max=sum;
+			}
+		}
+		return result;
+	}
+
+}
